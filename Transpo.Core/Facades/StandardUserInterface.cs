7508e6186c9d81524614c8465ad515c300d80931
Aleksandar Kuzmanoski <kuzmanoski.aleksandar@students.finki.ukim.mk>
2015-10-05 17:38:22 +0200

    Interfaces/UserInterface
    Креиран кориснички интерфејс на моделот. Целта на овој интерфејс е да го
    направи моделот едноставен те да има само една влезна точка која што ќе
    ја користат контролерите и погледите.

---
+++ Transpo.Core/Facades/StandardUserInterface.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Transpo.Core.Entities;
+
+namespace Transpo.Core.Facades
+{
+    class StandardUserInterface : UserInterface
+    {
+        private User user;
+        
+        public StandardUserInterface(User user) 
+        {
+            this.user = user;
+        }
+
+        public int getMyID()
+        {
+            return user.id;
+        }
+
+        public ICollection<Ride> getRides(CriticalPoint from, CriticalPoint to)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Transpo.Core/Interfaces/UserInterface.cs
@@ -0,0 +1,35 @@
+﻿// XMLsample.cs
+// compile with: /doc:XMLsample.xml
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Transpo.Core.Entities;
+
+namespace Transpo.Core.Facades
+{
+  
+    /// <summary>
+    /// This is the interface of the application for 
+    /// authenticated user.
+    /// 
+    /// </summary>
+    interface UserInterface
+    {
+        /// <summmary>
+        /// Calling this method will result with collection of rides
+        /// that pass the route specified with the parametars.
+        /// </summary>
+        /// <param name="from">The starting point of the route.</param>
+        /// <param name="to">The ending point of the route</param>
+        /// <returns>Collection of riddes that pass the asked route.</returns>
+        public ICollection<Ride> getRides(CriticalPoint from, CriticalPoint to);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns>Your id.</returns>
+        public int getMyID();
+    }
+}
