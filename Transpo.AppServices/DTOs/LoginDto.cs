d2300e1da9df2c63d5971c227c2098b218e4f504
martinj96 <martinjosifoski@yahoo.com>
2015-10-09 03:36:31 +0200

    Edit

---
+++ Transpo.AppServices/DTOs/LoginDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Transpo.AppServices.DTOs
+{
+    public class LoginDto
+    {
+        public string Name { get; set; }
+        public string AccessToken { get; set; }
+        public string Gender { get; set; }
+        public string Email { get; set; }
+        public string Link { get; set; }
+        public long FacebookId { get; set; }
+    }
+}
