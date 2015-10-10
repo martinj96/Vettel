b902d20af22b067e36c362800634e7c7db203145
martinj96 <martinjosifoski@yahoo.com>
2015-10-10 02:38:54 +0200

    New version

---
+++ Transpo.WebApp/Models/RideDetailsViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Transpo.Core.Entities;
+
+namespace Transpo.WebApp.Models
+{
+    public class RideDetailsViewModel
+    {
+        public RideViewModel Ride { get; set; }
+        public UserViewModel Driver { get; set; }
+        public bool UserHasPermission { get; set; }
+        public bool UserIsRider { get; set; }
+        public ICollection<UserViewModel> Riders { get; set; }
+        public ICollection<UserViewModel> RideRequesters { get; set; }
+    }
+}
---
+++ Transpo.WebApp/Models/RideViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Transpo.AppServices.DTOs;
+using Transpo.Core.Entities;
+
+namespace Transpo.WebApp.Models
+{
+    public class RideViewModel
+    {
+        public RideViewModel(Ride ride)
+        {
+            Departure = ride.Departure;
+            Description = ride.Description;
+            Detour = ride.Detour;
+            Length = ride.Length;
+            PricePerPassenger = ride.PricePerPassenger;
+            SeatsLeft = ride.SeatsLeft;
+        }
+        public decimal PricePerPassenger { get; set; }
+        public int SeatsLeft { get; set; }
+        public decimal Length { get; set; }
+        public int Detour { get; set; }
+        public DateTime Departure { get; set; }
+        public string Description { get; set; }
+        public List<CriticalPointDto> CriticalPoints { get; set; }
+    }
+}
